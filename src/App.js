edb2995ebd795d9d77f7832bd1ae6d306ac1d111
mikiimikii <hajorge2012@gmail.com>
2022-06-07 02:35:17 -0300

    add

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_FIRST_SECRET}&units=imperial`
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input 
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text"
+          />
+      </div>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>{data.name}</p>
+          </div>
+          <div className='temp'>
+            {data.main ? <h1>{data.main.temp.toFixed()}ºF</h1> : null}
+          </div>
+          <div className='description'>
+            {data.wether ? <h2>{data.wether[0].main}</h2> : null}
+          </div>
+        </div>
+        {data.name !== undefined && 
+          <div className='bottom'>
+            <div className='feels'>
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}</p> : null}
+              <p>Fells Like</p>
+            </div>
+            <div className='humidity'>
+              {data.main ? <p>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className='wind'>
+              {data.main ? <p>{data.wind.speed.toFixed()}PH</p> : null}
+              <p>Wind speed</p>
+            </div>
+          </div> 
+        }
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
